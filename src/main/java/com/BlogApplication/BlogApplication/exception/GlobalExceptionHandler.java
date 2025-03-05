ea56a2eba2fa979349c8e14acef3a670ff5d4c9c
aditya <raj488337@gmai.com>
2025-03-05 22:40:19 +0530

    commit

---
+++ src/main/java/com/BlogApplication/BlogApplication/exception/GlobalExceptionHandler.java
@@ -0,0 +1,12 @@
+package com.BlogApplication.BlogApplication.exception;
+
+import com.BlogApplication.BlogApplication.payload.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    public ResponseEntity<ApiResponse> resource
+
+}
